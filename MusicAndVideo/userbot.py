4d802eee819d803be802ceed0ed7fdd9a8bd6f2f
Source Freedom <104025850+SoRsRR8r9@users.noreply.github.com>
2022-04-20 18:12:47 +0300

    Update userbot.py

--- MusicAndVideo/userbot.py
+++ MusicAndVideo/userbot.py
@@ -30,22 +30,22 @@ async def _human_time_duration(seconds):
     return ", ".join(parts)
 
 
-@Client.on_message(filters.command(["ping"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["بنك"], prefixes=f"{HNDLR}"))
 async def ping(client, m: Message):
     await m.delete()
     start = time()
     current_time = datetime.utcnow()
-    m_reply = await m.reply_text("⚡")
+    m_reply = await m.reply_text("ثواني -› ")
     delta_ping = time() - start
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
     await m_reply.edit(
-        f"<b>🏓 PONG</b> `{delta_ping * 1000:.3f} ms` \n<b>⏳ AKTIF</b> - `{uptime}`"
+        f"<b>-›  بنك</b> `{delta_ping * 1000:.3f} ms` \n<b>-›  الوقت</b> - `{uptime}`"
     )
 
 
 @Client.on_message(
-    filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=f"{HNDLR}")
+    filters.user(SUDO_USERS) & filters.command(["اعادة تشغيل"], prefixes=f"{HNDLR}")
 )
 async def restart(client, m: Message):
     await m.delete()
@@ -58,85 +58,44 @@ async def restart(client, m: Message):
     await loli.edit("7")
     await loli.edit("8")
     await loli.edit("9")
-    await loli.edit("**✅ Userbot Di Mulai Ulang**")
+    await loli.edit("**-›  ياެمطوࢪي تم اެعادةه تشغيݪ اެݪحساب**")
     os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
 
 
-@Client.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["الاوامر"], prefixes=f"{HNDLR}"))
 async def help(client, m: Message):
     await m.delete()
     HELP = f"""
-<b>👋 Hallo {m.from_user.mention}!
+<b>هݪاެ بࢪۅ 🌵 {m.from_user.mention}!
 
-🛠 MENU BANTUAN
-
-⚡ PERINTAH UNTUK SEMUA ORANG
-• {HNDLR}play [judul lagu | link youtube | balas file audio] - untuk memutar lagu
-• {HNDLR}vplay [judul video | link youtube | balas file video] - untuk memutar video
-• {HNDLR}playlist untuk melihat daftar putar
-• {HNDLR}ping - untuk cek status
-• {HNDLR}help - untuk melihat daftar perintah
-
-⚡ PERINTAH UNTUK SEMUA ADMIN
-• {HNDLR}resume - untuk melanjutkan pemutaran lagu atau video
-• {HNDLR}pause - untuk untuk menjeda pemutaran lagu atau video
-• {HNDLR}skip - untuk melewati lagu atau video
-• {HNDLR}end - untuk mengakhiri pemutaran</b>
+🎗️ يمديك تستخدم ذي الاوامر في المجموعة والخاص 
+-›  {HNDLR}ش - بالرد على ملف صوتي او اسم اغنية 
+-›  {HNDLR}ف  - بالرد على مقطع فيديو او اسم فيديو
+-›  {HNDLR}الانتضار - لرؤية قائمة الانتضار
+-›  {HNDLR}بنك - لروية بنك الحساب
+-›  {HNDLR}الاوامر - لرؤية اوامر المشرفين
+-›  {HNDLR}بلش - لاستمرار الأغنية المتوقفة مؤقتا
+-›  {HNDLR}كتم - لكتم صوت الحساب
+-›  {HNDLR}تخ - لتخطي اغنية من الانتضار
+-›  {HNDLR}ايدي - لرؤية الايدي الخاص بك
+-›  {HNDLR}ك - لايقاف تشغيل جميع الاغاني</b>
 """
     await m.reply(HELP)
 
 
-@Client.on_message(filters.command(["repo"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["السورس"], prefixes=f"{HNDLR}"))
 async def repo(client, m: Message):
     await m.delete()
     REPO = f"""
-<b>👋 Hallo {m.from_user.mention}!
+<b> هݪاެ بࢪۅ 🌵 {m.from_user.mention}!
 
-🎶 Music Dan Video Player UserBot
+قناة السورس : @xl444
+مطور السورس : @rr8r9
 
-🤖 Telegram UserBot Untuk Memutar Lagu Dan Video Di Obrolan Suara Telegram.
-
-✨ Dipersembahkan Oleh 
-• [PyTgCalls](https://github.com/pytgcalls/pytgcalls)
-• [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-
-📝 Persyaratan
-• Python 3.8+
-• FFMPEG
-• Nodejs v16+
-
-🛠 MENU BANTUAN
-
-⚡ PERINTAH UNTUK SEMUA ORANG
-• `/play [judul lagu | link youtube | balas file audio]` - untuk memutar lagu
-• `/vplay [judul video | link youtube | balas file video]` - untuk memutar video
-• `/playlist` untuk melihat daftar putar
-• `/ping` - untuk cek status
-• `/help` - untuk melihat daftar perintah
-
-⚡ PERINTAH UNTUK SEMUA ADMIN
-• `/resume` - untuk melanjutkan pemutaran lagu atau video
-• `/pause` - untuk untuk menjeda pemutaran lagu atau video
-• `/skip` - untuk melewati lagu atau video
-• `/end` - untuk mengakhiri pemutaran
-
-💡 Deployment
-
-💜 Heroku
-
- [𝗗𝗘𝗣𝗟𝗢𝗬 𝗞𝗘 𝗛𝗘𝗥𝗢𝗞𝗨](https://heroku.com/deploy?template=https://github.com/XtomiSN/MusicAndVideoPlayer)
-
-📚 Variabel Yang Dibutuhkan
-• `API_ID` - Dapatkan Dari [my.telegram.org](https://my.telegram.org)
-• `API_HASH` - Dapatkan Dari [my.telegram.org](https://my.telegram.org)
-• `SESSION` - Sesi String Pyrogram. Dapatkan String Dari [Sini](https://replit.com/@GoodBoysExe/string-session?lite=1&outputonly=1)
-• `SUDO_USER` - ID Akun Telegram Yang Digunakan Sebagai Admin
-
-
-🔥 KREDIT 
-• [Dan](https://github.com/delivrance) untuk [Pyrogram](https://github.com/pyrogram/pyrogram)
-• [Laky](https://github.com/Laky-64) untuk [PyTgCalls](https://github.com/pytgcalls/pytgcalls)</b>
+🎗️ السورس  
+• [github](https://heroku.com/deploy?template=https://github.com/SoRsRR8r9/strong)
+• [Pyrogram](https://replit.com/@freedomSource/Session-Generator#main.py)
+</b>
 """
     await m.reply(REPO, disable_web_page_preview=True)
