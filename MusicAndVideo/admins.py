c47468aae61d2a2ac831b84a24bd88db87792b96
Source Freedom <104025850+SoRsRR8r9@users.noreply.github.com>
2022-04-20 17:57:22 +0300

    Update admins.py

--- MusicAndVideo/admins.py
+++ MusicAndVideo/admins.py
@@ -7,7 +7,7 @@ from MusicAndVideo.helpers.handlers import skip_current_song, skip_item
 from MusicAndVideo.helpers.queues import QUEUE, clear_queue
 
 
-@Client.on_message(filters.command(["skip"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["تخ"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def skip(client, m: Message):
     await m.delete()
@@ -15,17 +15,17 @@ async def skip(client, m: Message):
     if len(m.command) < 2:
         op = await skip_current_song(chat_id)
         if op == 0:
-            await m.reply("**❌ Tidak ada apapun didalam antrian untuk dilewati!**")
+            await m.reply("**معݪش ، ماެفي شي مشتغݪ ياެعيني 🌵.**")
         elif op == 1:
-            await m.reply("Antrian Kosong, Meninggalkan Obrolan Suara**")
+            await m.reply("معݪش ، ماެفي شي في اެݪاެنتضاެࢪ طݪعت من اެݪمكاެݪمةه ❤️‍🔥**")
         else:
             await m.reply(
-                f"**⏭ Melewati pemutaran** \n**🎧 Sekarang memutar** - [{op[0]}]({op[1]}) | `{op[2]}`",
+                f"**-›  اެبشࢪ عيني تم اެݪتخطي** \n**-›  اެݪحين ࢪاެح اެغني** - [{op[0]}]({op[1]}) | `{op[2]}`",
                 disable_web_page_preview=True,
             )
     else:
         skip = m.text.split(None, 1)[1]
-        OP = "**🗑️ Menghapus lagu-lagu berikut dari Antrian: -**"
+        OP = "**🗑️ تمت إزالة الأغاني التالية من قائمة الانتظار: -**"
         if chat_id in QUEUE:
             items = [int(x) for x in skip.split(" ") if x.isdigit()]
             items.sort(reverse=True)
@@ -41,7 +41,7 @@ async def skip(client, m: Message):
             await m.reply(OP)
 
 
-@Client.on_message(filters.command(["end", "stop"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["ك", "ايقاف"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def stop(client, m: Message):
     await m.delete()
@@ -50,14 +50,14 @@ async def stop(client, m: Message):
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-            await m.reply("**✅ Mengakhiri pemutaran**")
+            await m.reply("**اެهݪين عيني اެبشࢪ ۅقفت اެݪاެغنيةه اެݪحين 🌵.**")
         except Exception as e:
             await m.reply(f"**ERROR** \n`{e}`")
     else:
-        await m.reply("**❌ Tidak ada apapun yang sedang diputar!**")
+        await m.reply("**معݪش ، ماެفي شي مشتغݪ ياެعيني 🌵.**")
 
 
-@Client.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["بلش"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def pause(client, m: Message):
     await m.delete()
@@ -66,15 +66,15 @@ async def pause(client, m: Message):
         try:
             await call_py.pause_stream(chat_id)
             await m.reply(
-                f"**⏸ Pemutaran dijeda.**\n\n• Untuk melanjutkan pemutaran, gunakan perintah » {HNDLR}resume"
+                f"**-›  اެبشࢪ ياެعيني بݪشت اެغني من جديد.**\n\n-› اެذاެ تبي تۅقفهاެ كماެن اެكتب  {HNDLR} كتم"
             )
         except Exception as e:
             await m.reply(f"**ERROR** \n`{e}`")
     else:
-        await m.reply("** ❌ Tidak ada apapun yang sedang diputar!**")
+        await m.reply("**  معݪش ، ماެفي شي مشتغݪ ياެعيني 🌵.**")
 
 
-@Client.on_message(filters.command(["resume"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["وكف"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def resume(client, m: Message):
     await m.delete()
@@ -83,9 +83,9 @@ async def resume(client, m: Message):
         try:
             await call_py.resume_stream(chat_id)
             await m.reply(
-                f"**▶ Melanjutkan pemutaran yang dijeda**\n\n• Untuk menjeda pemutaran, gunakan perintah » {HNDLR}pause**"
+                f"**-›  ياެعيني عݪى نداࢪتك اެبشࢪ ۅقفت اެݪاެغنيةه**\n\n-› اެذاެ تبي تكمݪ اެݪاެغنيةه اكتب{HNDLR}بلش**"
             )
         except Exception as e:
             await m.reply(f"**ERROR** \n`{e}`")
     else:
-        await m.reply("**❌ Tidak ada apapun yang sedang dijeda!**")
+        await m.reply("**معݪش ، ماެفي شي مشتغݪ ياެعيني 🌵.**")
