22e1e76b0336ee486e8d74d470ade4fc5cb519f8
Source Freedom <104025850+SoRsRR8r9@users.noreply.github.com>
2022-04-20 20:12:22 +0300

    Update user_id.py

--- MusicAndVideo/user_id.py
+++ MusicAndVideo/user_id.py
@@ -15,10 +15,10 @@ async def showid(_, message: Message):
 
     elif chat_type in ["group", "supergroup"]:
         _id = ""
-        _id += "<b>-›  ايدي الدردشة</b>: " f"<code>{message.chat.id}</code>\n"
+        _id += "<b>-› ايدي الدردشة </b>: " f"<code>{message.chat.id}</code>\n"
         if message.reply_to_message:
             _id += (
-                "<b>-› ايدي العضو</b>: "
+                "<b>-› هذا ايديك</b>: "
                 f"<code>{message.reply_to_message.from_user.id}</code>\n"
             )
             file_info = get_file_id(message.reply_to_message)
